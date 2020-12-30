6ea2233d109ba9287a3fbf5c448b324768515b84
iMostfa <almme111@gmail.com>
2020-12-30 16:30:39 +0200

    updated version

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
+window.onload = onWindowLoad
+// import { MemoryGame } from './GameEngine.js';
 
 
-  class MemoryGame { 
+      class MemoryGame { 
 
     constructor(numberOfPairs, cardsType) { 
         this.numberOfPairs = numberOfPairs
@@ -17,7 +19,8 @@
 
 
     start() { 
-        this.setInitialState(this.options)
+        this.setInitialState(this.options);
+        console.log("i'm working");
     }
 
      setInitialState(withArray) { 
@@ -67,7 +70,7 @@
 // Extentions
 
 
-function shffledData(array) { 
+ function shffledData (array) { 
     // TODO: we need to review this https://stackoverflow.com/a/46545530/5253913
 
 return array
@@ -77,11 +80,6 @@ return array
 
 }
 
-
-
-
-window.onload = onWindowLoad()
-
 function onWindowLoad() { 
     var memoryGame = new MemoryGame(4, "")
 
@@ -89,9 +87,8 @@ function onWindowLoad() {
     this.memoryGame.start()
     this.cardElements = []
     this.sceneElements = []
-
     this.actions = [ ]
-
+    
     cardData = memoryGame.options;
     // shuffledCards = shffledData(cardData) ; 
     // setInitialState(shuffledCards) ; 
@@ -239,4 +236,87 @@ function provideCardsData() {
 
 }
 
+
+
+
+
+//MARK - GAME MODEL
+
+
+// class MemoryGame { 
+
+//     constructor(numberOfPairs, cardsType) { 
+//         this.numberOfPairs = numberOfPairs
+//         this.cardsType = cardsType // will support other types later 
+//         this.options = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,0x1F354];
+//         this.options = this.options
+//         .flatMap( emoji => [emoji, emoji])
+//         .reduce( function (initialValue, currentEmojiArray) { 
+//             return initialValue.concat(currentEmojiArray) 
+//             }, [])
+    
+//         this.options = shffledData(this.options)
+// }
+
+
+//     start() { 
+//         this.setInitialState(this.options)
+//     }
+
+//      setInitialState(withArray) { 
+//         this.totalOpend = 0  ; /* total opened cards which are not solved */
+//         this.currentlySelected = []
+//         this.currentlySelectedTitle = []
+//         this.cardElements = []
+//         this.actions = []
+    
+//         this.state = withArray.map ( (emoji, index) => {
+//             //TODO: we should have an id to keep track of things ??
+//             return { name: emoji, isFlipped: false, index: index, markedCorrectly: false}
+//         })
+        
+//     }
+
+//     select(cardTitle, cardIndex,withState, matchedCorrectlyCallback, notMatchedCallback) { 
+//         this.state[cardIndex].isFlipped = !withState
+//         console.log("card index " + cardIndex)
+//         this.currentlySelected.push(cardIndex)
+//         console.log(cardTitle)
+//         this.currentlySelectedTitle.push(cardTitle)
+
+
+//         if (this.currentlySelected.length == 2) { 
+            
+//             var firstSelectedCard = this.currentlySelectedTitle[0]
+//             var secondSelectedCard = this.currentlySelectedTitle[1]
+
+//             if (firstSelectedCard == secondSelectedCard) { 
+//                 console.log(this.currentlySelected)
+//                 matchedCorrectlyCallback(this.currentlySelected[0], this.currentlySelected[1])
+//                 this.currentlySelected = []
+//                 this.currentlySelectedTitle = []
+//             }else { 
+//                  notMatchedCallback(this.currentlySelected[0], this.currentlySelected[1])
+//                 this.currentlySelected= []
+//                 this.currentlySelectedTitle = []
+
+//             } 
+            
+//         }
+//     }
+// }
+
+
+// // Extentions
+
+
+// function shffledData(array) { 
+//     // TODO: we need to review this https://stackoverflow.com/a/46545530/5253913
+
+// return array
+// .map((a) => ({sort: Math.random(), value: a}))
+// .sort((a, b) => a.sort - b.sort)
+// .map((a) => a.value)
+
+// }
 
